ffa9441bd050429129a09d1d93a8e14c12eceb1c
Artem Minsadyrov <artem.mindsadyrov@actumdigital.com>
2022-06-13 12:40:12 +0200

    dashboard

---
+++ cypress/integration/submit.spec.js
@@ -0,0 +1,11 @@
+
+describe('Submit Form', () => {
+    it('should till amd submit developer name', () => {
+        cy.visit('https://devexpress.github.io/testcafe/example/')
+        cy.get('#developer-name').type('mike')
+        cy.get('#tried-test-cafe').click()
+        cy.get('#submit-button').click()
+        cy.get('#article-header').contains('Thank you')
+
+    })
+})
